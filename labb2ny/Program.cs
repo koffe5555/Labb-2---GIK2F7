9a0292f761e637de11c678b4b6e71cee31339e5b
koffe5555 <56026120+koffe5555@users.noreply.github.com>
2020-09-25 11:32:16 +0200

    Add files via upload

--- labb2ny/Program.cs
+++ labb2ny/Program.cs
@@ -7,11 +7,12 @@ namespace labb2ny
     {
         static void Main(string[] args)
         {
+            var mechanic = new Worker();
+            var emp = new List<Worker>();
+            emp.Add(mechanic);
             var depart = new List<Departments>();
             var workshop = new Departments();
             depart.Add(workshop);
-
-
         }
     }
 }
